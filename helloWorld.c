2186243cd199a935f28021a140e1402e7c06367d
KisoGit <maximilian.ackermann@gmx.de>
2019-12-07 18:03:53 +0100

    added calculator

--- helloWorld.c
+++ helloWorld.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
+/*
+    My first c program :)
+*/
 int main(void) {
 
     char str1[] = "Hi, what is your name?\n";
     int age = 22;
     double gpa = 3.7;
-        
+    const int PI_NUM = 3.14;            //constant int
+    char str2[20] = "";
+
 
     printf(str1);
-    //input = getstring();
-
+    scanf("%s", str2);                  //for other types &name, fgets(name, limitOfCharacters, stdin) to take a whole line of text.
+    printf("Your name is %s.\n", str2);
     
-    printf("Your name is ");
-    //putstring(input);
-    printf(" ?\n");
 
-    printf("%f", pow(2, 3)); //2^3
-    printf("%f", sqrt(4));   //square root
-    printf("%f", ceil(4.3456));   //rounds up
-    printf("%f", floor(4.3456));   //rounds down
+    printf("%f", pow(2, 3));            //2^3
+    printf("%f", sqrt(4));              //square root
+    printf("%f", ceil(4.3456));         //rounds up
+    printf("%f\n", floor(4.3456));      //rounds down
 
     return 0;
 }
