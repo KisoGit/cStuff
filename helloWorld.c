1e8062b68f01df50e7cf380fe8a1c00c5f891309
KisoGit <maximilian.ackermann@gmx.de>
2019-12-07 16:57:11 +0100

    added helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    char str1[] = "Hi, what is your name?\n";
+    int age = 22;
+    double gpa = 3.7;
+        
+
+    printf(str1);
+    //input = getstring();
+
+    
+    printf("Your name is ");
+    //putstring(input);
+    printf(" ?\n");
+
+    return 0;
+}
