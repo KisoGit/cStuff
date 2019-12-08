f1a7d9e146892f1f791af1606764b2a6736d90a2
KisoGit <maximilian.ackermann@gmx.de>
2019-12-08 13:53:16 +0100

    added comments

--- calculator.c
+++ calculator.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 /*
-    My calculator :)
+/    My calculator :)
 */
 int main(void) {
 
--- helloWorld.c
+++ helloWorld.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 /*
-    My first c program :)
+/    My first c program :)
 */
 int main(void) {
 
